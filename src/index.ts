93daf5d9e698514087bf829c3d82b6b3ed7fd4f7
Terry Su <theterrysu@163.com>
2019-12-24 21:44:14 +0800

    initialize

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import FS, { fstat } from 'fs-extra'
+import comparingWords from '../data/comparingWords'
+import knownWordsMap from '../data/knownWordsMap'
+import PATH from 'path'
+
+const outputNewWordsFile = PATH.resolve( __dirname, '../output/newWords.txt' )
+
+export const run = () => {
+  // let newWordsMap = {}
+  // let newWords = []
+  let newWordsStr = ''
+  for ( let word of comparingWords ) {
+    word = word.trim()
+    if ( knownWordsMap[ word ] === undefined ) {
+      // newWordsMap[ word ] = 0
+      // newWords.push( word )
+      newWordsStr = `${newWordsStr}${word}\n`
+    }
+  }
+  FS.outputFileSync( outputNewWordsFile, newWordsStr, { encoding: 'utf8' }  ) 
+}
+
+run()
---
+++ test/index.spec.ts
@@ -0,0 +1,8 @@
+import { run } from "../src/index"
+const assert = require( 'assert' )
+
+describe( "Test", () => {
+  it( "Unit", () => {
+    run()
+  } )
+} )
